e237bfff2c43a0aaca4e81333ee58beeb65b12cf
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-10-26 15:21:37 +0200

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,10 +1,37 @@
+def func1(acc, mass, vel, time):
+    a = 1/2 * acc * time * time + vel * time + 0
+    if a < 0:
+        return 0
+    else:
+        return a
 
 acceleration = input("Enter acceleration: ")
+try:
+    int(acceleration)
+    print('noice')
+except ValueError:
+    print('the fuck u doin')
+    acceleration = 0
+masse = input("Enter the mass: ")
+try:
+    int(masse)
+    print('noice')
+except ValueError:
+    print('the fuck u doin')
+    masse = 1
+velocity = input("Enter the velocity: ")
+try:
+    int(velocity)
+    print('noice')
+except ValueError:
+    print('the fuck u doin')
+    velocity = 0
+tid = input("Enter the time: ")
+try:
+    int(tid)
+    print('noice')
+except ValueError:
+    print('the fuck u doin')
+    tid = 1
 
-
-def func1(acc, mass, vel, time):
-    a = 0.5 * acc * time * time + vel * time + 0
-    return a
-
-
-print(func1(acceleration, 2, 3, 4))
+print(func1(int(acceleration), int(masse), int(velocity), int(tid)))
