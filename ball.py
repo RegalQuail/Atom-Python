695bcb6f69c1dda108a5d9cbadb4cebce6b8ea2c
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-10-26 15:24:56 +0200

    Update ball.py

--- ball.py
+++ ball.py
@@ -5,33 +5,37 @@ def func1(acc, mass, vel, time):
     else:
         return a
 
-acceleration = input("Enter acceleration: ")
-try:
-    int(acceleration)
-    print('noice')
-except ValueError:
-    print('the fuck u doin')
-    acceleration = 0
-masse = input("Enter the mass: ")
-try:
-    int(masse)
-    print('noice')
-except ValueError:
-    print('the fuck u doin')
-    masse = 1
-velocity = input("Enter the velocity: ")
-try:
-    int(velocity)
-    print('noice')
-except ValueError:
-    print('the fuck u doin')
-    velocity = 0
-tid = input("Enter the time: ")
-try:
-    int(tid)
-    print('noice')
-except ValueError:
-    print('the fuck u doin')
-    tid = 1
 
-print(func1(int(acceleration), int(masse), int(velocity), int(tid)))
+def getVariables():
+    acceleration = input("Enter acceleration: ")
+    try:
+        int(acceleration)
+        print('noice')
+    except ValueError:
+        print('the fuck u doin')
+        acceleration = 0
+    masse = input("Enter the mass: ")
+    try:
+        int(masse)
+        print('noice')
+    except ValueError:
+        print('the fuck u doin')
+        masse = 1
+    velocity = input("Enter the velocity: ")
+    try:
+        int(velocity)
+        print('noice')
+    except ValueError:
+        print('the fuck u doin')
+        velocity = 0
+    tid = input("Enter the time: ")
+    try:
+        int(tid)
+        print('noice')
+    except ValueError:
+        print('the fuck u doin')
+        tid = 1
+    
+    print(func1(int(acceleration), int(masse), int(velocity), int(tid)))
+
+getVariables()
